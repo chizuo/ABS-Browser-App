906dfad982ad06e4534b487d27226f229d021b91
Jonathan <jon.chua51@gmail.com>
2023-03-23 18:31:11 -0700

    created a sign in sheet

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,25 @@
+const account = localStorage.getItem("abs-account");
+
 function init() {
-    
+    console.log(account);
+    if(account !== null) {
+        console.log('account is not null');
+        $('.navbar-nav').html(`
+            <li class="nav-item ms-auto">
+                <a class="nav-link" href="#">Account Options</a>
+            </li>
+            <li class="nav-item ms-auto">
+                <a class="nav-link" href="#">Playlist Options</a>
+            </li>
+        `)
+    } else {
+        console.log('account is null');
+        $('.navbar-nav').html(`
+            <li class="nav-item ms-auto">
+              <a class="nav-link active" aria-current="page" href="login.html">Login</a>
+            </li>
+        `)
+    }
 }
 
-$(document).ready(init);
+$(document).ready(function() { init(); });
